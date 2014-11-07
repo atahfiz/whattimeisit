2e5d7d40095ad3a1b059dd55b85cb3433fe6ba24
Ahmad Tahfiz Ahmad Fauzi <atahfiz93@gmail.com>
2014-11-07 06:12:16 +0000

    Create Home and Users nav

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Workspace::Application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   get 'new' => 'users#new'
+  get 'home' => 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
